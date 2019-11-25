96ea4c70380cf5de64d903e2f3556c6bfd5c5b91
Andres Chaves <correodandres@gmail.com>
2019-11-25 17:40:33 -0500

    work day 6

--- Mod_1/Calculator_class/descriptive.py
+++ Mod_1/Calculator_class/descriptive.py
@@ -11,16 +11,16 @@ class Calculator:
 
     def __init__(self, data):
         self.data = data
-        self.set_data(self.data)
+        self.set_data()
 
     # Method that initalizes all attributes
 
-    def set_data(self, data):
-        self.length = len(data)
-        self.mean = self._calc_mean(data)
-        self.median = self._calc_median(data)
-        self.variance = self._calc_variance(data)
-        self.stand_dev = self._calc_std(data)
+    def set_data(self):
+        self.length = len(self.data)
+        self.mean = self._calc_mean()
+        self.median = self._calc_median()
+        self.variance = self._calc_variance()
+        self.stand_dev = self._calc_std()
 
     # Method that adds new values to the data list
 
@@ -34,38 +34,31 @@ class Calculator:
         self.data = [elem for elem in self.data if elem not in bad_data]
         self.set_data(self.data)
 
-    def _calc_mean(self, data):
-        return sum(data) / len(data)
+    def _calc_mean(self):
+        return sum(self.data) / self.length
 
     # function that returns the median
 
-    def _calc_median(self, data):
+    def _calc_median(self):
 
         # sort the data
-        data = sorted(data)
+        self.data = sorted(self.data)
 
         # Chech of odd number of values
-        if len(data) % 2 != 0:
-            return data[int(len(data) / 2)]
+        if len(self.data) % 2 != 0:
+            return self.data[int(len(self.data) / 2)]
 
         # Do the even case
-        return (data[(int(len(data) / 2)) - 1] +
-                data[(int(len(data) / 2))]) / 2
-
+        return (self.data[(int(len(self.data) / 2)) - 1] +
+                self.data[(int(len(self.data) / 2))]) / 2
     # variance fuction
 
-    def _calc_variance(self, data):
+    def _calc_variance(self):
 
-        return (sum([(x - self._calc_mean(data))**2 for x in data])) / \
-            (len(data) - 1)
+        return (sum([(x - self._calc_mean(self.data))**2 for x in self.data])) / \
+            (len(self.data) - 1)
 
     # standard deviation function
 
-    def _calc_std(self, data):
-        return (self._calc_variance(data))**.5
-
-
-data = [2, 20, 45, 15, 10, 55, 80]
-instance = Calculator(data)
-instance.remove_data([80])
-print(instance.data)
+    def _calc_std(self):
+        return (self._calc_variance(self.data))**.5
